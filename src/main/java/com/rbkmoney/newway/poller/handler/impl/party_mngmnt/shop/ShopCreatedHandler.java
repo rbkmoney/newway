561a91fc71e242db8257d044590e8e60c85e0dfe
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2018-08-30 16:51:15 +0300

    Removed method extracting

--- src/main/java/com/rbkmoney/newway/poller/handler/impl/party_mngmnt/shop/ShopCreatedHandler.java
+++ src/main/java/com/rbkmoney/newway/poller/handler/impl/party_mngmnt/shop/ShopCreatedHandler.java
@@ -30,59 +30,59 @@ public class ShopCreatedHandler extends AbstractClaimChangedHandler {
     @Transactional(propagation = Propagation.REQUIRED)
     public void handle(PartyChange change, Event event) {
         getClaimStatus(change).getAccepted().getEffects().stream()
-                .filter(e -> e.isSetShopEffect() && e.getShopEffect().getEffect().isSetCreated()).forEach(e -> processShopCreated(event, e));
-    }
-
-    private void processShopCreated(Event event, ClaimEffect e) {
-        long eventId = event.getId();
-        ShopEffectUnit shopEffect = e.getShopEffect();
-        Shop shopCreated = shopEffect.getEffect().getCreated();
-        String shopId = shopEffect.getShopId();
-        String partyId = event.getSource().getPartyId();
-        log.info("Start shop created handling, eventId={}, partyId={}, shopId={}", eventId, shopId);
-        com.rbkmoney.newway.domain.tables.pojos.Shop shop = new com.rbkmoney.newway.domain.tables.pojos.Shop();
-        shop.setEventId(eventId);
-        shop.setEventCreatedAt(TypeUtil.stringToLocalDateTime(event.getCreatedAt()));
-        shop.setShopId(shopId);
-        shop.setPartyId(partyId);
-        shop.setCreatedAt(TypeUtil.stringToLocalDateTime(shopCreated.getCreatedAt()));
-        com.rbkmoney.newway.domain.enums.Blocking blocking = TypeUtil.toEnumField(shopCreated.getBlocking().getSetField().getFieldName(), com.rbkmoney.newway.domain.enums.Blocking.class);
-        if (blocking == null) {
-            throw new IllegalArgumentException("Illegal shop blocking: " + shopCreated.getBlocking());
-        }
-        shop.setBlocking(blocking);
-        if (shopCreated.getBlocking().isSetUnblocked()) {
-            shop.setBlockingUnblockedReason(shopCreated.getBlocking().getUnblocked().getReason());
-            shop.setBlockingUnblockedSince(TypeUtil.stringToLocalDateTime(shopCreated.getBlocking().getUnblocked().getSince()));
-        } else if (shopCreated.getBlocking().isSetBlocked()) {
-            shop.setBlockingBlockedReason(shopCreated.getBlocking().getBlocked().getReason());
-            shop.setBlockingBlockedSince(TypeUtil.stringToLocalDateTime(shopCreated.getBlocking().getBlocked().getSince()));
-        }
-        com.rbkmoney.newway.domain.enums.Suspension suspension = TypeUtil.toEnumField(shopCreated.getSuspension().getSetField().getFieldName(), com.rbkmoney.newway.domain.enums.Suspension.class);
-        if (suspension == null) {
-            throw new IllegalArgumentException("Illegal shop suspension: " + shopCreated.getSuspension());
-        }
-        shop.setSuspension(suspension);
-        if (shopCreated.getSuspension().isSetActive()) {
-            shop.setSuspensionActiveSince(TypeUtil.stringToLocalDateTime(shopCreated.getSuspension().getActive().getSince()));
-        } else if (shopCreated.getSuspension().isSetSuspended()) {
-            shop.setSuspensionSuspendedSince(TypeUtil.stringToLocalDateTime(shopCreated.getSuspension().getSuspended().getSince()));
-        }
-        if (shopCreated.getLocation().isSetUrl()) {
-            shop.setLocationUrl(shopCreated.getLocation().getUrl());
-        } else {
-            throw new IllegalArgumentException("Illegal shop location " + shopCreated.getLocation());
-        }
-        shop.setCategoryId(shopCreated.getCategory().getId());
-        if (shopCreated.isSetAccount()) {
-            ShopUtil.fillShopAccount(shop, shopCreated.getAccount());
-        }
-        shop.setContractId(shopCreated.getContractId());
-        shop.setPayoutToolId(shopCreated.getPayoutToolId());
-        if (shopCreated.isSetPayoutSchedule()) {
-            shop.setPayoutScheduleId(shopCreated.getPayoutSchedule().getId());
-        }
-        shopDao.save(shop);
-        log.info("Shop has been saved, eventId={}, shopId={}", eventId, shopId);
+                .filter(e -> e.isSetShopEffect() && e.getShopEffect().getEffect().isSetCreated()).forEach(e -> {
+            long eventId = event.getId();
+            ShopEffectUnit shopEffect = e.getShopEffect();
+            Shop shopCreated = shopEffect.getEffect().getCreated();
+            String shopId = shopEffect.getShopId();
+            String partyId = event.getSource().getPartyId();
+            log.info("Start shop created handling, eventId={}, partyId={}, shopId={}", eventId, partyId, shopId);
+            com.rbkmoney.newway.domain.tables.pojos.Shop shop = new com.rbkmoney.newway.domain.tables.pojos.Shop();
+            shop.setEventId(eventId);
+            shop.setEventCreatedAt(TypeUtil.stringToLocalDateTime(event.getCreatedAt()));
+            shop.setShopId(shopId);
+            shop.setPartyId(partyId);
+            shop.setCreatedAt(TypeUtil.stringToLocalDateTime(shopCreated.getCreatedAt()));
+            com.rbkmoney.newway.domain.enums.Blocking blocking = TypeUtil.toEnumField(shopCreated.getBlocking().getSetField().getFieldName(), com.rbkmoney.newway.domain.enums.Blocking.class);
+            if (blocking == null) {
+                throw new IllegalArgumentException("Illegal shop blocking: " + shopCreated.getBlocking());
+            }
+            shop.setBlocking(blocking);
+            if (shopCreated.getBlocking().isSetUnblocked()) {
+                shop.setBlockingUnblockedReason(shopCreated.getBlocking().getUnblocked().getReason());
+                shop.setBlockingUnblockedSince(TypeUtil.stringToLocalDateTime(shopCreated.getBlocking().getUnblocked().getSince()));
+            } else if (shopCreated.getBlocking().isSetBlocked()) {
+                shop.setBlockingBlockedReason(shopCreated.getBlocking().getBlocked().getReason());
+                shop.setBlockingBlockedSince(TypeUtil.stringToLocalDateTime(shopCreated.getBlocking().getBlocked().getSince()));
+            }
+            com.rbkmoney.newway.domain.enums.Suspension suspension = TypeUtil.toEnumField(shopCreated.getSuspension().getSetField().getFieldName(), com.rbkmoney.newway.domain.enums.Suspension.class);
+            if (suspension == null) {
+                throw new IllegalArgumentException("Illegal shop suspension: " + shopCreated.getSuspension());
+            }
+            shop.setSuspension(suspension);
+            if (shopCreated.getSuspension().isSetActive()) {
+                shop.setSuspensionActiveSince(TypeUtil.stringToLocalDateTime(shopCreated.getSuspension().getActive().getSince()));
+            } else if (shopCreated.getSuspension().isSetSuspended()) {
+                shop.setSuspensionSuspendedSince(TypeUtil.stringToLocalDateTime(shopCreated.getSuspension().getSuspended().getSince()));
+            }
+            shop.setDetailsName(shopCreated.getDetails().getName());
+            shop.setDetailsDescription(shopCreated.getDetails().getDescription());
+            if (shopCreated.getLocation().isSetUrl()) {
+                shop.setLocationUrl(shopCreated.getLocation().getUrl());
+            } else {
+                throw new IllegalArgumentException("Illegal shop location " + shopCreated.getLocation());
+            }
+            shop.setCategoryId(shopCreated.getCategory().getId());
+            if (shopCreated.isSetAccount()) {
+                ShopUtil.fillShopAccount(shop, shopCreated.getAccount());
+            }
+            shop.setContractId(shopCreated.getContractId());
+            shop.setPayoutToolId(shopCreated.getPayoutToolId());
+            if (shopCreated.isSetPayoutSchedule()) {
+                shop.setPayoutScheduleId(shopCreated.getPayoutSchedule().getId());
+            }
+            shopDao.save(shop);
+            log.info("Shop has been saved, eventId={}, partyId={}, shopId={}", eventId, partyId, shopId);
+        });
     }
 }
