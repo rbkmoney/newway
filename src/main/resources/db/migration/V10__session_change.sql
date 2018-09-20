b729d35e546958d60a8ad811bc755877cb63dace
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2018-09-20 15:08:17 +0300

    Added refund session change

--- src/main/resources/db/migration/V10__session_change.sql
+++ src/main/resources/db/migration/V10__session_change.sql
@@ -13,8 +13,13 @@ ALTER TABLE nw.payment ADD COLUMN session_payload_transaction_bound_trx_extra_js
 ALTER TABLE nw.payment ADD COLUMN session_payload_proxy_state_changed_proxy_state BYTEA;
 ALTER TABLE nw.payment ADD COLUMN session_payload_interaction_requested_interaction_json CHARACTER VARYING;
 
-
-
-
-
-
+ALTER TABLE nw.refund ADD COLUMN session_target nw.session_target_status;
+ALTER TABLE nw.refund ADD COLUMN session_payload nw.session_change_payload;
+ALTER TABLE nw.refund ADD COLUMN session_payload_finished_result nw.session_change_payload_finished_result;
+ALTER TABLE nw.refund ADD COLUMN session_payload_finished_result_failed_failure_json CHARACTER VARYING;
+ALTER TABLE nw.refund ADD COLUMN session_payload_suspended_tag CHARACTER VARYING;
+ALTER TABLE nw.refund ADD COLUMN session_payload_transaction_bound_trx_id CHARACTER VARYING;
+ALTER TABLE nw.refund ADD COLUMN session_payload_transaction_bound_trx_timestamp TIMESTAMP WITHOUT TIME ZONE;
+ALTER TABLE nw.refund ADD COLUMN session_payload_transaction_bound_trx_extra_json CHARACTER VARYING;
+ALTER TABLE nw.refund ADD COLUMN session_payload_proxy_state_changed_proxy_state BYTEA;
+ALTER TABLE nw.refund ADD COLUMN session_payload_interaction_requested_interaction_json CHARACTER VARYING;
