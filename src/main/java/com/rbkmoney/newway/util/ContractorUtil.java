8dd1bccc34bf5edf42f9ac1b34b2fa5b893af6a6
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2018-09-21 15:21:52 +0300

    Added sql-script and util class

---
+++ src/main/java/com/rbkmoney/newway/util/ContractorUtil.java
@@ -0,0 +1,61 @@
+package com.rbkmoney.newway.util;
+
+import com.rbkmoney.damsel.domain.InternationalLegalEntity;
+import com.rbkmoney.damsel.domain.RussianLegalEntity;
+import com.rbkmoney.damsel.domain.RussianPrivateEntity;
+import com.rbkmoney.geck.common.util.TBaseUtil;
+import com.rbkmoney.geck.common.util.TypeUtil;
+import com.rbkmoney.newway.domain.enums.ContractorType;
+import com.rbkmoney.newway.domain.enums.LegalEntity;
+import com.rbkmoney.newway.domain.enums.PrivateEntity;
+import com.rbkmoney.newway.domain.tables.pojos.Contractor;
+
+public class ContractorUtil {
+    public static Contractor convertContractor(long eventId, String eventCreatedAt, String partyId, long partyRevision, com.rbkmoney.damsel.domain.Contractor contractorSource, String contractorId) {
+        Contractor contractor = new Contractor();
+        contractor.setEventId(eventId);
+        contractor.setEventCreatedAt(TypeUtil.stringToLocalDateTime(eventCreatedAt));
+        contractor.setRevision(partyRevision);
+        contractor.setPartyId(partyId);
+        contractor.setContractorId(contractorId);
+        contractor.setType(TBaseUtil.unionFieldToEnum(contractorSource, ContractorType.class));
+        if (contractorSource.isSetRegisteredUser()) {
+            contractor.setRegisteredUserEmail(contractorSource.getRegisteredUser().getEmail());
+        } else if (contractorSource.isSetLegalEntity()) {
+            contractor.setLegalEntity(TBaseUtil.unionFieldToEnum(contractorSource.getLegalEntity(), LegalEntity.class));
+            if (contractorSource.getLegalEntity().isSetRussianLegalEntity()) {
+                RussianLegalEntity russianLegalEntity = contractorSource.getLegalEntity().getRussianLegalEntity();
+                contractor.setRussianLegalEntityRegisteredName(russianLegalEntity.getRegisteredName());
+                contractor.setRussianLegalEntityRegisteredNumber(russianLegalEntity.getRegisteredNumber());
+                contractor.setRussianLegalEntityInn(russianLegalEntity.getInn());
+                contractor.setRussianLegalEntityActualAddress(russianLegalEntity.getActualAddress());
+                contractor.setRussianLegalEntityPostAddress(russianLegalEntity.getPostAddress());
+                contractor.setRussianLegalEntityRepresentativePosition(russianLegalEntity.getRepresentativePosition());
+                contractor.setRussianLegalEntityRepresentativeFullName(russianLegalEntity.getRepresentativeFullName());
+                contractor.setRussianLegalEntityRepresentativeDocument(russianLegalEntity.getRepresentativeDocument());
+                contractor.setRussianLegalEntityRussianBankAccount(russianLegalEntity.getRussianBankAccount().getAccount());
+                contractor.setRussianLegalEntityRussianBankName(russianLegalEntity.getRussianBankAccount().getBankName());
+                contractor.setRussianLegalEntityRussianBankPostAccount(russianLegalEntity.getRussianBankAccount().getBankPostAccount());
+                contractor.setRussianLegalEntityRussianBankBik(russianLegalEntity.getRussianBankAccount().getBankBik());
+            } else if (contractorSource.getLegalEntity().isSetInternationalLegalEntity()) {
+                InternationalLegalEntity internationalLegalEntity = contractorSource.getLegalEntity().getInternationalLegalEntity();
+                contractor.setInternationalLegalEntityLegalName(internationalLegalEntity.getLegalName());
+                contractor.setInternationalLegalEntityTradingName(internationalLegalEntity.getTradingName());
+                contractor.setInternationalLegalEntityRegisteredAddress(internationalLegalEntity.getRegisteredAddress());
+                contractor.setInternationalLegalEntityActualAddress(internationalLegalEntity.getActualAddress());
+                contractor.setInternationalLegalEntityRegisteredNumber(internationalLegalEntity.getRegisteredNumber());
+            }
+        } else if (contractorSource.isSetPrivateEntity()) {
+            contractor.setPrivateEntity(TBaseUtil.unionFieldToEnum(contractorSource.getPrivateEntity(), PrivateEntity.class));
+            if (contractorSource.getPrivateEntity().isSetRussianPrivateEntity()) {
+                RussianPrivateEntity russianPrivateEntity = contractorSource.getPrivateEntity().getRussianPrivateEntity();
+                contractor.setRussianPrivateEntityFirstName(russianPrivateEntity.getFirstName());
+                contractor.setRussianPrivateEntitySecondName(russianPrivateEntity.getSecondName());
+                contractor.setRussianPrivateEntityMiddleName(russianPrivateEntity.getMiddleName());
+                contractor.setRussianPrivateEntityPhoneNumber(russianPrivateEntity.getContactInfo().getPhoneNumber());
+                contractor.setRussianPrivateEntityEmail(russianPrivateEntity.getContactInfo().getEmail());
+            }
+        }
+        return contractor;
+    }
+}
