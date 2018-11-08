08a95e7cd480e8d0bea8cf83cf5c4312b65c4ec7
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2018-11-08 18:15:54 +0300

    Moved classes, added EventService interface

---
+++ src/main/java/com/rbkmoney/newway/service/EventService.java
@@ -0,0 +1,8 @@
+package com.rbkmoney.newway.service;
+
+import java.util.Optional;
+
+public interface EventService<TEvent, TPayload> {
+    Optional<Long> getLastEventId();
+    void handleEvents(TEvent processingEvent, TPayload payload);
+}
