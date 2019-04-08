43a90d90653f873f5afb54802d3d43df031ef0c2
d.baykov <d.baykov@rbkmoney.com>
2019-04-08 13:22:10 +0300

    Update payment_method_type ENUM to newway DB [2]

--- src/main/resources/db/migration/V27__1.0.43_add_empty_cvv_bank_card.sql
+++
@@ -1,3 +0,0 @@
-CREATE TYPE nw.empty_cvv_bank_card AS ENUM('visa', 'mastercard', 'visaelectron', 'maestro',
-                                           'forbrugsforeningen', 'dankort', 'amex', 'dinersclub',
-                                           'discover', 'unionpay', 'jcb', 'nspkmir');
