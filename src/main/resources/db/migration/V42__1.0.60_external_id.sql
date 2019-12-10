7ff4a4566a1b21163cef914f399ef3ad2710a854
Pavel Popov <tolkonepiu@users.noreply.github.com>
2019-12-10 17:14:47 +0300

    BJ-700: Add "if not exists" to last migration script (#95)

--- src/main/resources/db/migration/V42__1.0.60_external_id.sql
+++ src/main/resources/db/migration/V42__1.0.60_external_id.sql
@@ -1,8 +1,8 @@
-alter table nw.invoice add column external_id character varying;
-create index invoice_external_id_idx on nw.invoice(external_id) where external_id is not null;
+alter table nw.invoice add column if not exists external_id character varying;
+create index if not exists invoice_external_id_idx on nw.invoice(external_id) where external_id is not null;
 
-alter table nw.payment add column external_id character varying;
-create index payment_external_id_idx on nw.payment(external_id) where external_id is not null;
+alter table nw.payment add column if not exists external_id character varying;
+create index  if not exists payment_external_id_idx on nw.payment(external_id) where external_id is not null;
 
-alter table nw.refund add column external_id character varying;
-create index refund_external_id_idx on nw.refund(external_id) where external_id is not null;
+alter table nw.refund add column if not exists external_id character varying;
+create index if not exists refund_external_id_idx on nw.refund(external_id) where external_id is not null;
