53920111efafd601c86b9a4944f846ebe117ba6a
Pavel Popov <popoffpavel@yandex.ru>
2020-02-04 16:19:02 +0300

    Add 'if not exists' clause

--- src/main/resources/db/migration/V44__1.0.62_add_payment_bank_info_columns.sql
+++ src/main/resources/db/migration/V44__1.0.62_add_payment_bank_info_columns.sql
@@ -1,2 +1,2 @@
-alter table nw.payment add column payer_issuer_country character varying;
-alter table nw.payment add column payer_bank_name character varying;
+alter table nw.payment add column if not exists payer_issuer_country character varying;
+alter table nw.payment add column if not exists payer_bank_name character varying;
