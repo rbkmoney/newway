e8d7cfa00cc383944d57b7acd72d1de577adf513
k.struzhkin <k.struzhkin@MacBook-Pro-rbkadmin.local>
2020-03-18 19:32:17 +0300

    Add listener enable

--- src/main/java/com/rbkmoney/newway/config/KafkaConsumerBeanEnableConfig.java
+++ src/main/java/com/rbkmoney/newway/config/KafkaConsumerBeanEnableConfig.java
@@ -1,7 +1,9 @@
 package com.rbkmoney.newway.config;
 
 import com.rbkmoney.newway.poller.listener.InvoicingKafkaListener;
+import com.rbkmoney.newway.poller.listener.PartyManagementListener;
 import com.rbkmoney.newway.service.InvoicingService;
+import com.rbkmoney.newway.service.PartyManagementService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,5 +17,11 @@ public class KafkaConsumerBeanEnableConfig {
     @ConditionalOnProperty(value = "info.single-instance-mode", havingValue = "false")
     public InvoicingKafkaListener paymentEventsKafkaListener(InvoicingService invoicingService) {
         return new InvoicingKafkaListener(invoicingService);
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "info.single-instance-mode", havingValue = "false")
+    public PartyManagementListener partyManagementListener(PartyManagementService partyManagementService) {
+        return new PartyManagementListener(partyManagementService);
     }
 }
