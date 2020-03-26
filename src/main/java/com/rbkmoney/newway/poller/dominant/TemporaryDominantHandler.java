0f94322e060a3840b9e8926b6f446fc97a1d39a2
Inal Arsanukaev <in.all.young@gmail.com>
2020-03-26 12:44:47 +0300

    NEW-48: Removed temporary poller (#107)
    Co-authored-by: Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>

--- src/main/java/com/rbkmoney/newway/poller/dominant/TemporaryDominantHandler.java
+++
@@ -1,8 +0,0 @@
-package com.rbkmoney.newway.poller.dominant;
-
-public interface TemporaryDominantHandler<T> {
-
-    boolean acceptAndSet(T change);
-
-    void handle(T change, Long versionId);
-}
