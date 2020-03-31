bf628526a2bff5a85b8de54aa006ae48d0c22b39
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2020-04-01 01:53:37 +0300

    Fix after review

--- src/main/java/com/rbkmoney/newway/service/RecurrentPaymentToolService.java
+++ src/main/java/com/rbkmoney/newway/service/RecurrentPaymentToolService.java
@@ -1,5 +1,6 @@
 package com.rbkmoney.newway.service;
 
+import com.rbkmoney.damsel.payment_processing.RecurrentPaymentToolChange;
 import com.rbkmoney.damsel.payment_processing.RecurrentPaymentToolEventData;
 import com.rbkmoney.machinegun.eventsink.MachineEvent;
 import com.rbkmoney.newway.poller.event_stock.impl.recurrent_payment_tool.AbstractRecurrentPaymentToolHandler;
@@ -19,22 +20,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RequiredArgsConstructor
 public class RecurrentPaymentToolService {
 
-    private final List<AbstractRecurrentPaymentToolHandler> recurrentPaymentToolHandlers;
+    private final List<AbstractRecurrentPaymentToolHandler> handlers;
     private final MachineEventParser<RecurrentPaymentToolEventData> parser;
 
     @Transactional(propagation = Propagation.REQUIRED)
     public void handleEvents(List<MachineEvent> events) {
         events.forEach(event -> {
             AtomicInteger cnt = new AtomicInteger(0);
-            RecurrentPaymentToolEventData recurrentPaymentToolEventData = parser.parse(event);
-            recurrentPaymentToolEventData.getChanges().forEach(
-                    change -> recurrentPaymentToolHandlers.forEach
-                            (handler -> {
-                                if (handler.accept(change)) {
-                                    handler.handle(change, event, cnt.getAndIncrement());
-                                }
-                            }));
+            List<RecurrentPaymentToolChange> changes = parser.parse(event).getChanges();
+            changes.forEach(change -> {
+                for (var handler : handlers) {
+                    if (handler.accept(change)) {
+                        handler.handle(change, event, cnt.getAndIncrement());
+                        break;
+                    }
+                }
+            });
         });
-
     }
 }
