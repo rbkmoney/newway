4952e43a24108da1d10c8fe8373bff6d025f7649
vitaxa <vitaxa93gamebox@gmail.com>
2020-05-13 12:45:23 +0300

    BJ-890: add chargeback (#124)

---
+++ src/main/java/com/rbkmoney/newway/dao/invoicing/impl/ChargebackDaoImpl.java
@@ -0,0 +1,61 @@
+package com.rbkmoney.newway.dao.invoicing.impl;
+
+import com.rbkmoney.dao.impl.AbstractGenericDao;
+import com.rbkmoney.mapper.RecordRowMapper;
+import com.rbkmoney.newway.dao.invoicing.iface.ChargebackDao;
+import com.rbkmoney.newway.domain.tables.pojos.Chargeback;
+import com.rbkmoney.newway.domain.tables.records.ChargebackRecord;
+import com.rbkmoney.newway.exception.DaoException;
+import org.jooq.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.Optional;
+
+import static com.rbkmoney.newway.domain.tables.Chargeback.CHARGEBACK;
+
+@Component
+public class ChargebackDaoImpl extends AbstractGenericDao implements ChargebackDao {
+
+    private final RowMapper<Chargeback> chargebackRowMapper;
+
+    @Autowired
+    public ChargebackDaoImpl(DataSource dataSource) {
+        super(dataSource);
+        this.chargebackRowMapper = new RecordRowMapper<>(CHARGEBACK, Chargeback.class);
+    }
+
+    @Override
+    public Long save(Chargeback chargeback) throws DaoException {
+        ChargebackRecord chargebackRecord = getDslContext().newRecord(CHARGEBACK, chargeback);
+        Query query = getDslContext().insertInto(CHARGEBACK)
+                .set(chargebackRecord)
+                .onConflict(CHARGEBACK.INVOICE_ID, CHARGEBACK.SEQUENCE_ID, CHARGEBACK.CHANGE_ID)
+                .doNothing()
+                .returning(CHARGEBACK.ID);
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        execute(query, keyHolder);
+        return Optional.ofNullable(keyHolder.getKey()).map(Number::longValue).orElse(null);
+    }
+
+    @Override
+    public Chargeback get(String invoiceId, String paymentId, String chargebackId) throws DaoException {
+        Query query = getDslContext().selectFrom(CHARGEBACK)
+                .where(CHARGEBACK.INVOICE_ID.eq(invoiceId)
+                        .and(CHARGEBACK.PAYMENT_ID.eq(paymentId))
+                        .and(CHARGEBACK.CHARGEBACK_ID.eq(chargebackId))
+                        .and(CHARGEBACK.CURRENT));
+
+        return fetchOne(query, chargebackRowMapper);
+
+    }
+
+    @Override
+    public void updateNotCurrent(Long id) throws DaoException {
+        Query query = getDslContext().update(CHARGEBACK).set(CHARGEBACK.CURRENT, false).where(CHARGEBACK.ID.eq(id));
+        executeOne(query);
+    }
+}
---
+++ src/main/java/com/rbkmoney/newway/poller/event_stock/impl/invoicing/chargeback/InvoicePaymentChargebackCashFlowChangedHandler.java
@@ -0,0 +1,83 @@
+package com.rbkmoney.newway.poller.event_stock.impl.invoicing.chargeback;
+
+import com.rbkmoney.damsel.payment_processing.InvoiceChange;
+import com.rbkmoney.damsel.payment_processing.InvoicePaymentChange;
+import com.rbkmoney.damsel.payment_processing.InvoicePaymentChargebackCashFlowChanged;
+import com.rbkmoney.damsel.payment_processing.InvoicePaymentChargebackChange;
+import com.rbkmoney.geck.common.util.TypeUtil;
+import com.rbkmoney.geck.filter.Filter;
+import com.rbkmoney.geck.filter.PathConditionFilter;
+import com.rbkmoney.geck.filter.condition.IsNullCondition;
+import com.rbkmoney.geck.filter.rule.PathConditionRule;
+import com.rbkmoney.machinegun.eventsink.MachineEvent;
+import com.rbkmoney.newway.dao.invoicing.iface.CashFlowDao;
+import com.rbkmoney.newway.dao.invoicing.iface.ChargebackDao;
+import com.rbkmoney.newway.domain.enums.PaymentChangeType;
+import com.rbkmoney.newway.domain.tables.pojos.CashFlow;
+import com.rbkmoney.newway.domain.tables.pojos.Chargeback;
+import com.rbkmoney.newway.exception.NotFoundException;
+import com.rbkmoney.newway.poller.event_stock.impl.invoicing.AbstractInvoicingHandler;
+import com.rbkmoney.newway.service.CashFlowService;
+import com.rbkmoney.newway.util.CachbackUtil;
+import com.rbkmoney.newway.util.CashFlowUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class InvoicePaymentChargebackCashFlowChangedHandler extends AbstractInvoicingHandler {
+
+    private static final Filter CONDITION_FILTER = new PathConditionFilter(new PathConditionRule(
+            "invoice_payment_change.payload.invoice_payment_chargeback_change.payload.invoice_payment_chargeback_cash_flow_changed",
+            new IsNullCondition().not()));
+
+    private final ChargebackDao chargebackDao;
+    private final CashFlowService cashFlowService;
+    private final CashFlowDao cashFlowDao;
+
+    @Override
+    public void handle(InvoiceChange change, MachineEvent event, Integer changeId) {
+        long sequenceId = event.getEventId();
+        String invoiceId = event.getSourceId();
+        InvoicePaymentChange invoicePaymentChange = change.getInvoicePaymentChange();
+        String paymentId = invoicePaymentChange.getId();
+        InvoicePaymentChargebackChange invoicePaymentChargebackChange =
+                invoicePaymentChange.getPayload().getInvoicePaymentChargebackChange();
+        InvoicePaymentChargebackCashFlowChanged invoicePaymentChargebackCashFlowChanged =
+                invoicePaymentChargebackChange.getPayload().getInvoicePaymentChargebackCashFlowChanged();
+        String chargebackId = invoicePaymentChargebackChange.getId();
+
+        log.info("Start chargeback cash flow changed handling, sequenceId={}, invoiceId={}, paymentId={}, chargebackId={}",
+                sequenceId, invoiceId, paymentId, chargebackId);
+
+        Chargeback chargebackSource = chargebackDao.get(invoiceId, paymentId, chargebackId);
+        if (chargebackSource == null) {
+            throw new NotFoundException(String.format("Chargeback not found, invoiceId='%s', paymentId='%s', chargebackId='%s'",
+                    invoiceId, paymentId, chargebackId));
+        }
+
+        Long chargebackSourceId = chargebackSource.getId();
+        CachbackUtil.resetBaseFields(chargebackSource, event, changeId, sequenceId);
+        Long savedChargebackId = chargebackDao.save(chargebackSource);
+        if (savedChargebackId != null) {
+            chargebackDao.updateNotCurrent(savedChargebackId);
+            cashFlowService.save(chargebackSourceId, savedChargebackId, PaymentChangeType.chargeback);
+            List<CashFlow> cashFlows = CashFlowUtil.convertCashFlows(
+                    invoicePaymentChargebackCashFlowChanged.getCashFlow(),
+                    savedChargebackId,
+                    PaymentChangeType.chargeback);
+            cashFlowDao.save(cashFlows);
+        }
+        log.info("Chargeback cash flow changed have been succeeded, sequenceId={}, invoiceId={}, paymentId={}, chargebackId={}",
+                sequenceId, invoiceId, paymentId, chargebackId);
+    }
+
+    @Override
+    public Filter<InvoiceChange> getFilter() {
+        return CONDITION_FILTER;
+    }
+}
