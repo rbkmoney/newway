98579ccbbf577c6dd236a2c896017f23f0a6145a
vitaxa <vitaxa93gamebox@gmail.com>
2020-05-27 10:34:27 +0300

    add more adjustment cashflow sum (#131)

--- src/main/java/com/rbkmoney/newway/dao/invoicing/impl/AdjustmentDaoImpl.java
+++ src/main/java/com/rbkmoney/newway/dao/invoicing/impl/AdjustmentDaoImpl.java
@@ -3,19 +3,16 @@ package com.rbkmoney.newway.dao.invoicing.impl;
 import com.rbkmoney.dao.impl.AbstractGenericDao;
 import com.rbkmoney.mapper.RecordRowMapper;
 import com.rbkmoney.newway.dao.invoicing.iface.AdjustmentDao;
-import com.rbkmoney.newway.domain.enums.PaymentChangeType;
 import com.rbkmoney.newway.domain.tables.pojos.Adjustment;
 import com.rbkmoney.newway.domain.tables.records.AdjustmentRecord;
 import com.rbkmoney.newway.exception.DaoException;
 import org.jooq.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-
 import java.util.Optional;
 
 import static com.rbkmoney.newway.domain.Tables.ADJUSTMENT;
@@ -53,17 +50,6 @@ public class AdjustmentDaoImpl extends AbstractGenericDao implements AdjustmentD
                         .and(ADJUSTMENT.CURRENT));
 
         return fetchOne(query, adjustmentRowMapper);
-    }
-
-    @Override
-    public void updateCommissions(Long adjId) throws DaoException {
-        MapSqlParameterSource params = new MapSqlParameterSource("adjId", adjId).addValue("objType", PaymentChangeType.adjustment.name());
-        this.getNamedParameterJdbcTemplate().update(
-                "UPDATE nw.adjustment SET fee = (SELECT nw.get_adjustment_fee(nw.cash_flow.*) FROM nw.cash_flow WHERE obj_id = :adjId AND obj_type = CAST(:objType as nw.payment_change_type)), " +
-                        "provider_fee = (SELECT nw.get_adjustment_provider_fee(nw.cash_flow.*) FROM nw.cash_flow WHERE obj_id = :adjId AND obj_type = CAST(:objType as nw.payment_change_type)), " +
-                        "external_fee = (SELECT nw.get_adjustment_external_fee(nw.cash_flow.*) FROM nw.cash_flow WHERE obj_id = :adjId AND obj_type = CAST(:objType as nw.payment_change_type)) " +
-                        "WHERE  id = :adjId",
-                params);
     }
 
     @Override
---
+++ src/main/resources/db/migration/V60__add_adjustment_cashflow_amount.sql
@@ -0,0 +1,17 @@
+ALTER TABLE nw.adjustment
+    ADD amount BIGINT;
+
+UPDATE nw.adjustment a
+SET amount = p.fee - a.fee
+FROM nw.payment p
+WHERE p.payment_id = a.payment_id
+  AND p.invoice_id = a.invoice_id
+  AND p.current;
+
+ALTER TABLE nw.adjustment
+    ALTER COLUMN amount SET NOT NULL;
+
+ALTER TABLE nw.adjustment
+    DROP COLUMN fee,
+    DROP COLUMN external_fee,
+    DROP COLUMN provider_fee;
