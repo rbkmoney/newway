bde898691e04a540716ec1a5c4ef5ff52574c947
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2020-06-29 14:12:48 +0300

    Fix test

--- src/test/java/com/rbkmoney/newway/TestData.java
+++ src/test/java/com/rbkmoney/newway/TestData.java
@@ -1,6 +1,7 @@
 package com.rbkmoney.newway;
 
 import com.rbkmoney.damsel.domain.*;
+import com.rbkmoney.damsel.domain.InvoicePaymentChargeback;
 import com.rbkmoney.damsel.payment_processing.*;
 import com.rbkmoney.fistful.cashflow.FinalCashFlow;
 import com.rbkmoney.geck.common.util.TypeUtil;
