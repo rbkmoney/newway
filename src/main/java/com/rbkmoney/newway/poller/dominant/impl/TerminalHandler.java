758e10a196f99feeefc6f0e336ba57fa2f10fc22
vitaxa <vitaxa93gamebox@gmail.com>
2020-08-03 15:54:27 +0300

    BJ-971: provider unified (#145)

--- src/main/java/com/rbkmoney/newway/poller/dominant/impl/TerminalHandler.java
+++ src/main/java/com/rbkmoney/newway/poller/dominant/impl/TerminalHandler.java
@@ -52,6 +52,9 @@ public class TerminalHandler extends AbstractDominantHandler<TerminalObject, Ter
         if (data.isSetTerms()) {
             terminal.setTermsJson(JsonUtil.tBaseToJsonString(data.getTerms()));
         }
+        terminal.setExternalTerminalId(data.getExternalTerminalId());
+        terminal.setExternalMerchantId(data.getExternalMerchantId());
+        terminal.setMcc(data.getMcc());
         terminal.setCurrent(current);
         return terminal;
     }
