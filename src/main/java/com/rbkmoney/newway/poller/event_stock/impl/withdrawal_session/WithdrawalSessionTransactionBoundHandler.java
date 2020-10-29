1a79c93b4eab3648a5e7482d71d136eb54a6e009
d.baykov <d.baykov@rbkmoney.com>
2020-10-29 14:11:09 +0300

    NEW-55: Add WithdrawalSessionTransactionBoundHandler

--- src/main/java/com/rbkmoney/newway/poller/event_stock/impl/withdrawal_session/WithdrawalSessionTransactionBoundHandler.java
+++ src/main/java/com/rbkmoney/newway/poller/event_stock/impl/withdrawal_session/WithdrawalSessionTransactionBoundHandler.java
@@ -1,15 +1,14 @@
 package com.rbkmoney.newway.poller.event_stock.impl.withdrawal_session;
 
+import com.rbkmoney.fistful.base.TransactionInfo;
 import com.rbkmoney.fistful.withdrawal_session.Change;
 import com.rbkmoney.fistful.withdrawal_session.SinkEvent;
-import com.rbkmoney.geck.common.util.TBaseUtil;
 import com.rbkmoney.geck.common.util.TypeUtil;
 import com.rbkmoney.geck.filter.Filter;
 import com.rbkmoney.geck.filter.PathConditionFilter;
 import com.rbkmoney.geck.filter.condition.IsNullCondition;
 import com.rbkmoney.geck.filter.rule.PathConditionRule;
 import com.rbkmoney.newway.dao.withdrawal_session.iface.WithdrawalSessionDao;
-import com.rbkmoney.newway.domain.enums.WithdrawalSessionStatus;
 import com.rbkmoney.newway.domain.tables.pojos.WithdrawalSession;
 import com.rbkmoney.newway.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
@@ -21,18 +20,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class WithdrawalSessionFinishedHandler extends AbstractWithdrawalSessionHandler {
+public class WithdrawalSessionTransactionBoundHandler extends AbstractWithdrawalSessionHandler {
 
     private final WithdrawalSessionDao withdrawalSessionDao;
 
     private final Filter filter = new PathConditionFilter(
-            new PathConditionRule("finished", new IsNullCondition().not())
+            new PathConditionRule("transaction_bound", new IsNullCondition().not())
     );
 
     @Override
     @Transactional(propagation = Propagation.REQUIRED)
     public void handle(Change change, SinkEvent event) {
-        log.info("Start withdrawal session next state handling (eventId={}, sessionId={})",
+        log.info("Start withdrawal session transaction bound handling (eventId={}, sessionId={})",
                 event.getId(), event.getSource());
         WithdrawalSession withdrawalSession = withdrawalSessionDao.get(event.getSource());
         withdrawalSession.setId(null);
@@ -43,15 +42,22 @@ public class WithdrawalSessionFinishedHandler extends AbstractWithdrawalSessionH
         withdrawalSession.setEventOccuredAt(TypeUtil.stringToLocalDateTime(event.getPayload().getOccuredAt()));
         withdrawalSession.setWithdrawalSessionId(event.getSource());
 
-        withdrawalSession.setWithdrawalSessionStatus(TBaseUtil.unionFieldToEnum(change.getFinished(), WithdrawalSessionStatus.class));
-        if (change.getFinished().isSetFailed()) {
-            withdrawalSession.setFailureJson(JsonUtil.tBaseToJsonString(change.getFinished().getFailed()));
+        TransactionInfo trxInfo = change.getTransactionBound().getTrxInfo();
+        withdrawalSession.setTranInfoId(trxInfo.getId());
+        if (trxInfo.isSetTimestamp()) {
+            withdrawalSession.setTranInfoTimestamp(TypeUtil.stringToLocalDateTime(trxInfo.getTimestamp()));
+        }
+        withdrawalSession.setTranInfoJson(JsonUtil.objectToJsonString(trxInfo.getExtra()));
+        if (trxInfo.isSetAdditionalInfo()) {
+            withdrawalSession.setTranAdditionalInfoRrn(trxInfo.getAdditionalInfo().getRrn());
+            withdrawalSession.setTranAdditionalInfoJson(JsonUtil.tBaseToJsonString(trxInfo.getAdditionalInfo()));
         }
         withdrawalSessionDao.updateNotCurrent(event.getSource());
         long id = withdrawalSessionDao.save(withdrawalSession);
 
-        log.info("Withdrawal session state have been changed (Id={}, eventId={}, sourceId={}, status={})",
-                id, event.getId(), event.getSource(), withdrawalSession.getWithdrawalSessionStatus());
+        log.info("Withdrawal session transaction bound have been changed (Id={}, eventId={}, " +
+                        "sourceId={}, status={})", id, event.getId(), event.getSource(),
+                withdrawalSession.getWithdrawalSessionStatus());
     }
 
     @Override
