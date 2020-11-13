6ea16db5f7ba3836f58cfa661de526ca7c6ef934
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2020-11-13 12:56:17 +0300

    BJ-1012: Add PaymentRoutingRules dominant event processing (#166)

---
+++ src/main/java/com/rbkmoney/newway/poller/dominant/impl/PaymentRoutingRulesHandler.java
@@ -0,0 +1,57 @@
+package com.rbkmoney.newway.poller.dominant.impl;
+
+import com.rbkmoney.damsel.domain.RoutingRulesObject;
+import com.rbkmoney.damsel.domain.RoutingRuleset;
+import com.rbkmoney.newway.dao.dominant.iface.DomainObjectDao;
+import com.rbkmoney.newway.domain.tables.pojos.PaymentRoutingRule;
+import com.rbkmoney.newway.poller.dominant.AbstractDominantHandler;
+import com.rbkmoney.newway.util.JsonUtil;
+import lombok.RequiredArgsConstructor;
+import org.jooq.JSONB;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentRoutingRulesHandler
+        extends AbstractDominantHandler<RoutingRulesObject, PaymentRoutingRule, Integer> {
+
+    private final DomainObjectDao<PaymentRoutingRule, Integer> paymentRoutingRulesDao;
+
+    @Override
+    protected boolean acceptDomainObject() {
+        return getDomainObject().isSetPaymentRoutingRules();
+    }
+
+    @Override
+    public PaymentRoutingRule convertToDatabaseObject(RoutingRulesObject rulesObject,
+                                                      Long versionId,
+                                                      boolean current) {
+        PaymentRoutingRule paymentRoutingRule = new PaymentRoutingRule();
+        paymentRoutingRule.setRuleId(rulesObject.getRef().getId());
+
+        RoutingRuleset ruleset = rulesObject.getData();
+        paymentRoutingRule.setName(ruleset.getName());
+        paymentRoutingRule.setDescription(ruleset.getDescription());
+        paymentRoutingRule.setRoutingDecisionsJsonb(JSONB.valueOf(
+                JsonUtil.tBaseToJsonString(ruleset.getDecisions()))
+        );
+        paymentRoutingRule.setCurrent(current);
+        return paymentRoutingRule;
+    }
+
+    @Override
+    protected DomainObjectDao<PaymentRoutingRule, Integer> getDomainObjectDao() {
+        return paymentRoutingRulesDao;
+    }
+
+    @Override
+    protected RoutingRulesObject getObject() {
+        return getDomainObject().getPaymentRoutingRules();
+    }
+
+    @Override
+    protected Integer getObjectRefId() {
+        return getObject().getRef().getId();
+    }
+
+}
---
+++ src/test/java/com/rbkmoney/newway/poller/dominant/impl/PaymentRouningRulesHandlerTest.java
@@ -0,0 +1,64 @@
+package com.rbkmoney.newway.poller.dominant.impl;
+
+import com.rbkmoney.damsel.domain.*;
+import com.rbkmoney.newway.dao.dominant.impl.PaymentRoutingRulesDaoImpl;
+import com.rbkmoney.newway.domain.tables.pojos.PaymentRoutingRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PaymentRouningRulesHandlerTest {
+
+    @Mock
+    private PaymentRoutingRulesDaoImpl paymentRoutingRulesDao;
+
+    @Before
+    public void setUp() throws Exception {
+        paymentRoutingRulesDao = Mockito.mock(PaymentRoutingRulesDaoImpl.class);
+    }
+
+    @Test
+    public void convertToDatabaseObjectTest() {
+        RoutingRulesObject paymentRoutingRulesObject = buildPaymentRoutingRulesObject();
+        PaymentRoutingRulesHandler handler = new PaymentRoutingRulesHandler(paymentRoutingRulesDao);
+        handler.setDomainObject(DomainObject.payment_routing_rules(paymentRoutingRulesObject));
+
+        PaymentRoutingRule paymentRoutingRule =
+                handler.convertToDatabaseObject(paymentRoutingRulesObject, 1L, true);
+
+        assertNotNull(paymentRoutingRule);
+        assertEquals(paymentRoutingRule.getRuleId().intValue(), paymentRoutingRulesObject.getRef().getId());
+        assertEquals(paymentRoutingRule.getName(), paymentRoutingRulesObject.getData().getName());
+        assertEquals(paymentRoutingRule.getDescription(), paymentRoutingRulesObject.getData().getDescription());
+        assertFalse(paymentRoutingRule.getRoutingDecisionsJsonb() == null);
+    }
+
+    private RoutingRulesObject buildPaymentRoutingRulesObject() {
+        List<RoutingCandidate> candidates = new ArrayList<>();
+        Predicate predicate = new Predicate();
+        predicate.setConstant(true);
+        RoutingCandidate candidate = new RoutingCandidate()
+                .setDescription("CN-1")
+                .setAllowed(predicate)
+                .setTerminal(new TerminalRef().setId(1234))
+                .setWeight(12)
+                .setPriority(432);
+        candidates.add(candidate);
+        RoutingDecisions paymentRoutingDecisions = new RoutingDecisions();
+        paymentRoutingDecisions.setCandidates(candidates);
+        return new RoutingRulesObject()
+                .setRef(new RoutingRulesetRef().setId(123))
+                .setData(new RoutingRuleset()
+                        .setName("test")
+                        .setDescription("some desc")
+                        .setDecisions(paymentRoutingDecisions)
+                );
+    }
+
+}
