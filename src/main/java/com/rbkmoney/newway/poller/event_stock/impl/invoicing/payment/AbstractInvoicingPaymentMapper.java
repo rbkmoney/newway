c4394104af1bc31b79787fa3f2156c47b179530a
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2020-11-16 18:58:54 +0300

    Fixed after dev testing, remove dumb tests

--- src/main/java/com/rbkmoney/newway/poller/event_stock/impl/invoicing/payment/AbstractInvoicingPaymentMapper.java
+++ src/main/java/com/rbkmoney/newway/poller/event_stock/impl/invoicing/payment/AbstractInvoicingPaymentMapper.java
@@ -8,11 +8,21 @@ import com.rbkmoney.newway.poller.event_stock.impl.invoicing.AbstractInvoicingMa
 public abstract class AbstractInvoicingPaymentMapper extends AbstractInvoicingMapper<PaymentWrapper> {
 
     protected void setInsertProperties(Payment payment, Long sequenceId, Integer changeId, String eventCreatedAt){
-        payment.setWtime(null);
         payment.setId(null);
+        payment.setWtime(null);
         payment.setChangeId(changeId);
         payment.setSequenceId(sequenceId);
         payment.setEventCreatedAt(TypeUtil.stringToLocalDateTime(eventCreatedAt));
+        payment.setSessionPayloadTransactionBoundTrxExtraJson(null);
+        payment.setTrxAdditionalInfoAcsUrl(null);
+        payment.setTrxAdditionalInfoPareq(null);
+        payment.setTrxAdditionalInfoMd(null);
+        payment.setTrxAdditionalInfoTermUrl(null);
+        payment.setTrxAdditionalInfoPares(null);
+        payment.setTrxAdditionalInfoCavv(null);
+        payment.setTrxAdditionalInfoXid(null);
+        payment.setTrxAdditionalInfoCavvAlgorithm(null);
+        payment.setTrxAdditionalInfoThreeDsVerification(null);
     }
 
     protected void setUpdateProperties(Payment payment, String eventCreatedAt){
--- src/main/java/com/rbkmoney/newway/service/PaymentSquashService.java
+++ src/main/java/com/rbkmoney/newway/service/PaymentSquashService.java
@@ -1,6 +1,7 @@
 package com.rbkmoney.newway.service;
 
 import com.rbkmoney.newway.model.PaymentWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class PaymentSquashService {
 
@@ -18,16 +20,18 @@ public class PaymentSquashService {
         Iterator<Long> iterator = ids.iterator();
         groupedMap.forEach((key, pwList) -> {
             setIds(pwList, iterator);
-            squashById(pwList, result);
-            long count = result.stream().filter(w -> !w.isShouldInsert()).count();
+            List<PaymentWrapper> squashedById = squashById(pwList);
+            long count = squashedById.stream().filter(w -> !w.isShouldInsert()).count();
             if (count > 1) {
-                throw new IllegalStateException("Must be less or equal than one update statements");
+                throw new IllegalStateException("Must be less or equal than one update statements, " + pwList);
             }
+            result.addAll(squashedById);
         });
         return result;
     }
 
-    private void squashById(List<PaymentWrapper> pwList, List<PaymentWrapper> result) {
+    private List<PaymentWrapper> squashById(List<PaymentWrapper> pwList) {
+        List<PaymentWrapper> result = new ArrayList<>();
         var groupedById = pwList.stream().collect(Collectors.groupingBy(this::getId, LinkedHashMap::new, Collectors.toList()));
         groupedById.forEach((id, ws) -> {
             boolean shouldInsert = ws.get(0).isShouldInsert();
@@ -35,6 +39,7 @@ public class PaymentSquashService {
             pwLast.setShouldInsert(shouldInsert);
             result.add(pwLast);
         });
+        return result;
     }
 
     private void setIds(List<PaymentWrapper> pwList, Iterator<Long> iterator) {
