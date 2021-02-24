a89870f1804134e1b31a7955fc395b1a9be80bd0
karleowne <a.karlov@rbkmoney.com>
2021-02-24 20:41:19 +0700

    add default value 0

--- src/main/resources/db/migration/V81__fix_deposit_adjustment_and_revert.sql
+++ src/main/resources/db/migration/V81__fix_deposit_adjustment_and_revert.sql
@@ -5,21 +5,13 @@ ALTER TABLE nw.deposit_adjustment
     ALTER COLUMN currency_code DROP NOT NULL;
 
 ALTER TABLE nw.deposit_adjustment
-    ADD COLUMN party_revision BIGINT;
-ALTER TABLE nw.deposit_adjustment
-    ALTER COLUMN party_revision SET NOT NULL;
+    ADD COLUMN party_revision BIGINT NOT NULL DEFAULT 0;
 
 ALTER TABLE nw.deposit_adjustment
-    ADD COLUMN domain_revision BIGINT;
-ALTER TABLE nw.deposit_adjustment
-    ALTER COLUMN domain_revision SET NOT NULL;
+    ADD COLUMN domain_revision BIGINT NOT NULL DEFAULT 0;
 
 ALTER TABLE nw.deposit_revert
-    ADD COLUMN party_revision BIGINT;
-ALTER TABLE nw.deposit_revert
-    ALTER COLUMN party_revision SET NOT NULL;
+    ADD COLUMN party_revision BIGINT NOT NULL DEFAULT 0;
 
 ALTER TABLE nw.deposit_revert
-    ADD COLUMN domain_revision BIGINT;
-ALTER TABLE nw.deposit_revert
-    ALTER COLUMN domain_revision SET NOT NULL;
+    ADD COLUMN domain_revision BIGINT NOT NULL DEFAULT 0;
