8a8021003d3bc4cac3ac479f849c4ba9095848d0
malkoas <41993717+malkoas@users.noreply.github.com>
2021-03-24 18:34:37 +0300

    fix tests

--- src/test/java/com/rbkmoney/newway/dao/DepositRevertDaoTest.java
+++ src/test/java/com/rbkmoney/newway/dao/DepositRevertDaoTest.java
@@ -8,6 +8,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import static io.github.benas.randombeans.api.EnhancedRandom.random;
+import static junit.framework.TestCase.assertNull;
+import static org.junit.Assert.assertEquals;
 
 @Transactional
 public class DepositRevertDaoTest extends AbstractAppDaoTests {
