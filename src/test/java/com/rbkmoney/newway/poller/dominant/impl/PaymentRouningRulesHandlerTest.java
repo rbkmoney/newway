193175417e0c568ab8dbaceecedf8e18b48c3128
malkoas <41993717+malkoas@users.noreply.github.com>
2021-03-25 15:57:16 +0300

    fix tests

--- src/test/java/com/rbkmoney/newway/poller/dominant/impl/PaymentRouningRulesHandlerTest.java
+++ src/test/java/com/rbkmoney/newway/poller/dominant/impl/PaymentRouningRulesHandlerTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
