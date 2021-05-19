916890416b98f2fd19062cbf4db9e42bb264ad8a
karleowne <karleowne@gmail.com>
2021-05-19 16:49:39 +0700

    JD-277: add string type for mobile_operator field

--- src/main/resources/db/migration/V83__change_mobile_operator_type.sql
+++ src/main/resources/db/migration/V83__change_mobile_operator_type.sql
@@ -1,2 +1,5 @@
-alter table nw.recurrent_payment_tool alter column mobile_commerce_operator type character varying;
-alter table nw.payment alter column payer_mobile_operator type character varying;
+alter table nw.recurrent_payment_tool rename column mobile_commerce_operator to mobile_commerce_operator_legacy;
+alter table nw.recurrent_payment_tool add column mobile_commerce_operator character varying;
+
+alter table nw.payment rename column payer_mobile_operator to payer_mobile_operator_legacy;
+alter table nw.payment add column payer_mobile_operator character varying;
