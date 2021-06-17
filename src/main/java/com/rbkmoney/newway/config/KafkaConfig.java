867d140668fcc9b9c3388ecd268f4d41291676c4
NPospolita <nikita7asics@gmail.com>
2021-06-17 12:43:30 +0300

    Removed deprecated method

--- src/main/java/com/rbkmoney/newway/config/KafkaConfig.java
+++ src/main/java/com/rbkmoney/newway/config/KafkaConfig.java
@@ -181,7 +181,6 @@ public class KafkaConfig {
                                  ConcurrentKafkaListenerContainerFactory<String, T> factory) {
         factory.setConsumerFactory(consumerFactory);
         factory.setBatchListener(true);
-        factory.getContainerProperties();
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         factory.setBatchErrorHandler(kafkaErrorHandler());
         factory.setConcurrency(threadsNumber);
